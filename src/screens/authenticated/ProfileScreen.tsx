f132465561016a8d106f68e961b3501b5d31b410
Kent Yap <160380043+Ekentyap911@users.noreply.github.com>
2025-07-28 21:37:26 +0800

    change mobile regex validation

--- src/screens/authenticated/ProfileScreen.tsx
+++ src/screens/authenticated/ProfileScreen.tsx
@@ -45,9 +45,9 @@ const ProfileScreen = () => {
       errors.fullname = 'Fullname is required';
     }
 
-    const phoneRegex = /^01[0-9]{8,9}$/; // Phone number validation (Malaysian format)
+    const phoneRegex = /^\+?\d{1,20}$/; // Phone number validation (Malaysian format)
     if (formData.mobile && !phoneRegex.test(formData.mobile)) {
-      errors.mobile = 'Please enter a valid Malaysian mobile phone number';
+      errors.mobile = 'Please enter a valid mobile phone number, only plus (+) symbol and number are accepted';
     }
 
     setValidationErrors(errors);
@@ -126,6 +126,7 @@ const ProfileScreen = () => {
           disabled={isLoading}
           returnKeyType="done"
           onSubmitEditing={handleUpdateProfile}
+          maxLength={20}
         />
         {validationErrors.mobile && <HelperText type="error">{validationErrors.mobile}</HelperText>}
       </View>
--- src/screens/guest/SignupScreen.tsx
+++ src/screens/guest/SignupScreen.tsx
@@ -92,10 +92,10 @@ const SignupScreen = () => {
         errors.fullname = 'Fullname is required';
       }
 
-      const phoneRegex = /^01[0-9]{8,9}$/; // Phone number validation (Malaysian format)
-      if (formData.mobile && !phoneRegex.test(formData.mobile)) {
-        errors.mobile = 'Please enter a valid Malaysian mobile phone number';
-      }
+     const phoneRegex = /^\+?\d{1,20}$/; // Phone number validation (Malaysian format)
+     if (formData.mobile && !phoneRegex.test(formData.mobile)) {
+       errors.mobile = 'Please enter a valid mobile phone number, only plus (+) symbol and number are accepted';
+     }
     }
 
     setValidationErrors(errors);
@@ -231,6 +231,7 @@ const SignupScreen = () => {
               disabled={isLoading}
               returnKeyType="done"
               onSubmitEditing={handleCreateProfile}
+              maxLength={20}
             />
             {validationErrors.mobile && (
               <HelperText type="error">{validationErrors.mobile}</HelperText>
